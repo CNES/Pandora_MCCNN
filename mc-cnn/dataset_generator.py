937075c3e0802064c0c78ef11fe32ad324dda215
Veronique Defonte <veronique.defonte@c-s.fr>
2019-12-05 10:31:48 +0100

    Optimize dataset generator

--- mc-cnn/dataset_generator.py
+++ mc-cnn/dataset_generator.py
@@ -26,14 +26,24 @@ class MiddleburyGenerator(data.Dataset):
         self.data = None
         self.h5_file_image = h5py.File(image, 'r')
         self.patch_size = 11
+        self.image = []
+        self.id_image = []
 
         with h5py.File(file, 'r') as h5_file:
-            self.nb_dataset = len(h5_file)
+
             for dst in h5_file.keys():
                 if self.data is None:
                     self.data = h5_file[dst][:]
                 else:
                     self.data = np.concatenate((self.data, h5_file[dst][:]), axis=0)
+                self.id_image.append(int(h5_file[dst][0, 0]))
+
+        with h5py.File(image, 'r') as h5_file:
+            for grp in self.id_image:
+                image_grp = []
+                for dst in h5_file[str(int(grp))].keys():
+                    image_grp.append(h5_file[str(int(grp))][dst][:])
+                self.image.append(image_grp)
 
     def __getitem__(self, index):
         """
@@ -47,11 +57,13 @@ class MiddleburyGenerator(data.Dataset):
         y = int(self.data[index, 1])
         x = int(self.data[index, 2])
         disp = int(self.data[index, 3])
+        id_data = self.id_image.index(id_image)
 
-        nb_illuminations = len(self.h5_file_image[str(id_image)])
+        nb_illuminations = len(self.image[id_data])
         light_l = random.randint(0, nb_illuminations-1)
 
-        nb_exposures = self.h5_file_image[str(id_image)][str(light_l)].shape[0]
+        # nb_exposures = self.h5_file_image[str(id_image)][str(light_l)].shape[0]
+        nb_exposures = self.image[id_data][light_l].shape[0]
         exp_l = random.randint(0, nb_exposures-1)
 
         # Right illuminations and exposures
@@ -63,31 +75,31 @@ class MiddleburyGenerator(data.Dataset):
         if np.random.uniform() < 0.2:
             light_r = random.randint(0, nb_illuminations-1)
 
-            if exp_r > self.h5_file_image[str(id_image)][str(light_r)].shape[0] - 1:
-                exp_r = random.randint(0, self.h5_file_image[str(id_image)][str(light_r)].shape[0] - 1)
+            if exp_r > self.image[id_data][light_r].shape[0] - 1:
+                exp_r = random.randint(0, self.image[id_data][light_r].shape[0] - 1)
 
         if np.random.uniform() < 0.2:
-            nb_exposures = self.h5_file_image[str(id_image)][str(light_r)].shape[0]
+            nb_exposures = self.image[id_data][light_r].shape[0]
             exp_r = random.randint(0, nb_exposures-1)
 
         # Make the left patch
         w = int(self.patch_size / 2)
-        left = self.h5_file_image[str(id_image)][str(light_l)][exp_l, 0, y - w: y + w + 1, x - w: x + w + 1]
+        left = self.image[id_data][light_l][exp_l, 0, y - w: y + w + 1, x - w: x + w + 1]
 
         x_pos = -1
-        width = self.h5_file_image[str(id_image)][str(light_l)].shape[3]
+        width = self.image[id_data][light_l].shape[3]
         while x_pos < 0 or x_pos >= width:
             x_pos = int((x - disp) + np.random.uniform(-0.5, 0.5))
 
         x_neg = -1
         while x_neg < 0 or x_neg >= width:
-            x_neg = int((x - disp) + np.random.uniform(1.5, 6))
+            x_neg = int((x - disp) + np.random.uniform(1, 6))
 
         # Make the right positive patch
-        right_pos = self.h5_file_image[str(id_image)][str(light_r)][exp_r, 1, y - w: y + w + 1, x_pos - w: w + x_pos + 1]
+        right_pos = self.image[id_data][light_r][exp_r, 1, y - w: y + w + 1, x_pos - w: w + x_pos + 1]
 
         # Make the right negative patch
-        right_neg = self.h5_file_image[str(id_image)][str(light_r)][exp_r, 1, y - w: y + w + 1, x_neg - w: w + x_neg + 1]
+        right_neg = self.image[id_data][light_r][exp_r, 1, y - w: y + w + 1, x_neg - w: w + x_neg + 1]
 
         return np.stack((left, right_pos, right_neg), axis=0)
 
