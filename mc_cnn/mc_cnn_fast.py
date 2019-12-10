c355f9c4094291a8fc635bbe59b76b05cd1661f5
Véronique DEFONTE <defontev@visu05.sis.cnes.fr>
2019-12-10 15:01:17 +0000

    Add evaluation mode in fast forward function

--- mc_cnn/mc_cnn_fast.py
+++ mc_cnn/mc_cnn_fast.py
@@ -7,6 +7,7 @@
 """
 
 import torch.nn as nn
+import torch
 import torch.nn.functional as F
 
 
@@ -38,26 +39,48 @@ class FastMcCnn(nn.Module):
                       kernel_size=self.conv_kernel_size),
         )
 
-    def forward(self, sample):
+    def forward(self, sample, training):
         """
         Forward function
 
         :param sample: sample
-        :type sample: torch ( batch_size, 3, 11, 11) with : 3 is the left patch, right positive patch, right negative
-        patch, 11 the patch size
-        :return: left, right positive and right negative features
-        :rtype : tuple([batch_size, 64, 1, 1], [batch_size, 64, 1, 1], [batch_size, 64, 1, 1])
+        :type sample:
+        if training mode :
+            - torch ( batch_size, 3, 11, 11) with : 3 is the left patch, right positive patch, right negative
+               patch, 11 the patch
+        else:
+            - image torch(row, col)
+
+        :param training: training mode
+        :type training: bool
+        :return:
+        if training mode :
+            - left, right positive and right negative features
+        else:
+            - extracted features
+        :rtype :
+        if training mode :
+            - tuple([batch_size, 64, 1, 1], [batch_size, 64, 1, 1], [batch_size, 64, 1, 1])
+        else:
+            - torch(64, row, col)
         """
-        left = self.conv_blocks(sample[:, 0:1, :, :])
-        # left of shape : torch.Size([batch_size, 64, 1, 1])
-        left = F.normalize(left, p=2, dim=1)
+        if training:
+            left = self.conv_blocks(sample[:, 0:1, :, :])
+            # left of shape : torch.Size([batch_size, 64, 1, 1])
+            left = F.normalize(left, p=2, dim=1)
 
-        pos = self.conv_blocks(sample[:, 1:2, :, :])
-        # pos of shape : torch.Size([batch_size, 64, 1, 1])
-        pos = F.normalize(pos, p=2, dim=1)
+            pos = self.conv_blocks(sample[:, 1:2, :, :])
+            # pos of shape : torch.Size([batch_size, 64, 1, 1])
+            pos = F.normalize(pos, p=2, dim=1)
 
-        neg = self.conv_blocks(sample[:, 2:3, :, :])
-        # neg of shape : torch.Size([batch_size, 64, 1, 1])
-        neg = F.normalize(neg, p=2, dim=1)
+            neg = self.conv_blocks(sample[:, 2:3, :, :])
+            # neg of shape : torch.Size([batch_size, 64, 1, 1])
+            neg = F.normalize(neg, p=2, dim=1)
 
-        return left, pos, neg
+            return left, pos, neg
+
+        # Testing mode
+        else:
+            # Because input shape of nn.Conv2d is (Batch_size, Channel, H, W), we add 2 dimensions
+            features = self.conv_blocks(sample.unsqueeze(0).unsqueeze(0))
+            return torch.squeeze(F.normalize(features, p=2, dim=1))
