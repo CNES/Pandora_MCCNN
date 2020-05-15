1e8208847ba8e82aab769fa6bc1cb211f34175d1
Veronique Defonte <veronique.defonte@c-s.fr>
2020-05-15 11:03:01 +0200

    test!: test mc_cnn accurate

--- mc_cnn/mc_cnn_accurate.py
+++ mc_cnn/mc_cnn_accurate.py
@@ -125,7 +125,7 @@ class AccMcCnnTesting(nn.Module):
 
     def forward(self, ref, sec, disp_min, disp_max):
         """
-        Computes the cost volume for a pair of images
+        Extract reference and secondary features and computes the cost volume for a pair of images
 
         :param ref: reference image (normalized)
         :type ref: torch (row, col)
@@ -141,19 +141,38 @@ class AccMcCnnTesting(nn.Module):
         # Disabling gradient calculation in evaluation mode. It will reduce memory consumption
         with torch.no_grad():
             # Because input shape of nn.Conv2d is (Batch_size, Channel, H, W), we add 2 dimensions
+            # Shape ref_features and sec_features is [1, 112, row-10, col-10]
             ref_features = self.conv_blocks(ref.unsqueeze(0).unsqueeze(0))
             sec_features = self.conv_blocks(sec.unsqueeze(0).unsqueeze(0))
-            # Shape ref_features and sec_features is [1, 112, row-10, col-10]
 
-            _, _, _, nx_ref = ref_features.shape
-            _, _, _, nx_sec = sec_features.shape
+            cv = self.computes_cost_volume_mc_cnn_accurate(ref_features, sec_features, disp_min, disp_max, self.compute_cost_mc_cnn_accurate)
 
-            disparity_range = torch.arange(disp_min, disp_max + 1)
+            return cv
 
-            # Allocate the numpy cost volume cv = (disp, col, row), for efficient memory management
-            cv = np.zeros((len(disparity_range), ref_features.shape[3], ref_features.shape[2]), dtype=np.float32)
-            cv += np.nan
+    def computes_cost_volume_mc_cnn_accurate(self, ref_features, sec_features, disp_min, disp_max, measure):
+        """
+        Computes the cost volume using the reference and secondary features computing by mc_cnn accurate
 
+        :param ref_features: reference features
+        :type ref_features: Tensor of shape (1, 112, row, col)
+        :param sec_features: secondary features
+        :type sec_features: Tensor of shape (1, 112, 64, row, col)
+        :param measure: measure to apply
+        :type measure: function
+        :return: the cost volume ( similarity score is converted to a matching cost )
+        :rtype: 3D np.array (row, col, disp)
+        """
+        disparity_range = torch.arange(disp_min, disp_max + 1)
+
+        # Allocate the numpy cost volume cv = (disp, col, row), for efficient memory management
+        cv = np.zeros((len(disparity_range), ref_features.shape[3], ref_features.shape[2]), dtype=np.float32)
+        cv += np.nan
+
+        _, _, _, nx_ref = ref_features.shape
+        _, _, _, nx_sec = sec_features.shape
+
+        # Disabling gradient calculation in evaluation mode. It will reduce memory consumption
+        with torch.no_grad():
             for disp in disparity_range:
                 # range in the reference image
                 p = (max(0 - disp, 0), min(nx_ref - disp, nx_ref))
@@ -161,13 +180,26 @@ class AccMcCnnTesting(nn.Module):
                 q = (max(0 + disp, 0), min(nx_sec + disp, nx_sec))
                 d = int(disp - disp_min)
 
-                sample = torch.cat((ref_features[:, :, :, p[0]:p[1]], sec_features[:, :, :, q[0]:q[1]]), dim=1)
-                sample = torch.squeeze(sample)
-                # Tanspose because input of nn.Linear is(batch_size, *, in_features)
-                sample = self.fl_blocks(torch.transpose(sample, 0, 2))
-                sample = torch.squeeze(sample)
-                cv[d, p[0]:p[1], :] = sample.cpu().detach().numpy()
+                cv[d, p[0]:p[1], :] = np.swapaxes(measure(ref_features[:, :, :, p[0]:p[1]], sec_features[:, :, :, q[0]:q[1]]), 0, 1)
 
-            # The minus sign converts the similarity score to a matching cost
-            cv *= -1
-            return np.swapaxes(cv, 0, 2)
+        # The minus sign converts the similarity score to a matching cost
+        cv *= -1
+
+        return np.swapaxes(cv, 0, 2)
+
+    def compute_cost_mc_cnn_accurate(self, ref_features, sec_features):
+        """
+        Compute the cost between the reference and secondary features using the last part of the mc_cnn accurate
+
+        :param ref_features: reference features
+        :type ref_features: Tensor of shape (1, 112, row, col)
+        :param sec_features: secondary features
+        :type sec_features: Tensor of shape (1, 112, row, col)
+        :return: the cost
+        :rtype:  Tensor of shape (col, row)
+        """
+        sample = torch.cat((ref_features, sec_features), dim=1)
+        # Tanspose because input of nn.Linear is(batch_size, *, in_features)
+        sample = self.fl_blocks(sample.permute(0, 2, 3, 1))
+        sample = torch.squeeze(sample)
+        return sample.cpu().detach().numpy()
--- test/test_mc_cnn.py
+++ test/test_mc_cnn.py
@@ -11,6 +11,7 @@ import torch
 import torch.nn as nn
 
 from mc_cnn.run import computes_cost_volume_mc_cnn_fast
+from mc_cnn.mc_cnn_accurate import AccMcCnnTesting
 
 
 class TestMCCNN(unittest.TestCase):
@@ -53,6 +54,169 @@ class TestMCCNN(unittest.TestCase):
         cv_GT *= -1
 
         cv = computes_cost_volume_mc_cnn_fast(ref_feature, sec_features, -2, 2)
+
+        # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
+        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+
+    def test_computes_cost_volume_mc_cnn_fast_negative_disp(self):
+        """"
+        Test the computes_cost_volume_mc_cnn_fast function with negative disparities
+
+        """
+        # create reference and secondary features
+        ref_feature = torch.randn((64, 4, 4), dtype=torch.float64)
+        sec_features = torch.randn((64, 4, 4), dtype=torch.float64)
+
+        cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+
+        # Create the ground truth cost volume (row, col, disp)
+        cv_GT = np.full((4, 4, 4), np.nan)
+
+        # disparity -4
+        # all nan
+        # disparity -3
+        cv_GT[:, 3:, 1] = cos(ref_feature[:, :, 3:], sec_features[:, :, 0:1]).cpu().detach().numpy()
+        # disparity -2
+        cv_GT[:, 2:, 2] = cos(ref_feature[:, :, 2:], sec_features[:, :, 0:2]).cpu().detach().numpy()
+        # disparity -1
+        cv_GT[:, 1:, 3] = cos(ref_feature[:, :, 1:], sec_features[:, :, 0:3]).cpu().detach().numpy()
+
+        # The minus sign converts the similarity score to a matching cost
+        cv_GT *= -1
+
+        cv = computes_cost_volume_mc_cnn_fast(ref_feature, sec_features, -4, -1)
+
+        # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
+        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+
+    def test_computes_cost_volume_mc_cnn_fast_positive_disp(self):
+        """"
+        Test the computes_cost_volume_mc_cnn_fast function with positive disparities
+
+        """
+        # create reference and secondary features
+        ref_feature = torch.randn((64, 4, 4), dtype=torch.float64)
+        sec_features = torch.randn((64, 4, 4), dtype=torch.float64)
+
+        cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+
+        # Create the ground truth cost volume (row, col, disp)
+        cv_GT = np.full((4, 4, 4), np.nan)
+
+        # disparity 1
+        cv_GT[:, :3, 0] = cos(ref_feature[:, :, :3], sec_features[:, :, 1:4]).cpu().detach().numpy()
+        # disparity 2
+        cv_GT[:, :2, 1] = cos(ref_feature[:, :, :2], sec_features[:, :, 2:4]).cpu().detach().numpy()
+        # disparity 3
+        cv_GT[:, :1, 2] = cos(ref_feature[:, :, :1], sec_features[:, :, 3:]).cpu().detach().numpy()
+        # disparity 4
+        # all nan
+
+        # The minus sign converts the similarity score to a matching cost
+        cv_GT *= -1
+
+        cv = computes_cost_volume_mc_cnn_fast(ref_feature, sec_features, 1, 4)
+
+        # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
+        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+
+    def sad_cost(self, ref_features, sec_features):
+        """
+        Useful to test the computes_cost_volume_mc_cnn_accurate function
+        """
+        return torch.sum(abs(ref_features[0, :, :, :] - sec_features[0, :, :, :]), dim=0)
+
+    def test_computes_cost_volume_mc_cnn_accurate(self):
+        """"
+        Test the computes_cost_volume_mc_cnn_accurate function
+
+        """
+        # create reference and secondary features
+        ref_feature = torch.randn((1, 112, 4, 4), dtype=torch.float64)
+        sec_features = torch.randn((1, 112, 4, 4), dtype=torch.float64)
+
+        # Create the ground truth cost volume (row, col, disp)
+        cv_GT = np.full((4, 4, 5), np.nan)
+
+        # disparity -2
+        cv_GT[:, 2:, 0] = self.sad_cost(ref_feature[:, :, :, 2:], sec_features[:, :, :, 0:2]).cpu().detach().numpy()
+        # disparity -1
+        cv_GT[:, 1:, 1] = self.sad_cost(ref_feature[:, :, :, 1:], sec_features[:, :, :, 0:3]).cpu().detach().numpy()
+        # disparity 0
+        cv_GT[:, :, 2] = self.sad_cost(ref_feature[:, :, :, :], sec_features[:, :, :, :]).cpu().detach().numpy()
+        # disparity 1
+        cv_GT[:, :3, 3] = self.sad_cost(ref_feature[:, :, :, :3], sec_features[:, :, :, 1:4]).cpu().detach().numpy()
+        # disparity 2
+        cv_GT[:, :2, 4] = self.sad_cost(ref_feature[:, :, :, :2], sec_features[:, :, :, 2:4]).cpu().detach().numpy()
+
+        # The minus sign converts the similarity score to a matching cost
+        cv_GT *= -1
+
+        acc = AccMcCnnTesting()
+        # Because input shape of nn.Conv2d is (Batch_size, Channel, H, W), we add 1 dimensions
+        cv = acc.computes_cost_volume_mc_cnn_accurate(ref_feature, sec_features, -2, 2, self.sad_cost)
+
+        # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
+        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+
+    def test_computes_cost_volume_mc_cnn_accuratenegative_disp(self):
+        """"
+        Test the computes_cost_volume_mc_cnn_accurate function with negative disparities
+
+        """
+        # create reference and secondary features
+        ref_feature = torch.randn((1, 112, 4, 4), dtype=torch.float64)
+        sec_features = torch.randn((1, 112, 4, 4), dtype=torch.float64)
+
+        # Create the ground truth cost volume (row, col, disp)
+        cv_GT = np.full((4, 4, 4), np.nan)
+
+        # disparity -4
+        # all nan
+        # disparity -3
+        cv_GT[:, 3:, 1] = self.sad_cost(ref_feature[:, :, :, 3:], sec_features[:, :, :, 0:1]).cpu().detach().numpy()
+        # disparity -2
+        cv_GT[:, 2:, 2] = self.sad_cost(ref_feature[:, :, :, 2:], sec_features[:, :, :, 0:2]).cpu().detach().numpy()
+        # disparity -1
+        cv_GT[:, 1:, 3] = self.sad_cost(ref_feature[:, :, :, 1:], sec_features[:, :, :, 0:3]).cpu().detach().numpy()
+
+        # The minus sign converts the similarity score to a matching cost
+        cv_GT *= -1
+
+        acc = AccMcCnnTesting()
+        # Because input shape of nn.Conv2d is (Batch_size, Channel, H, W), we add 1 dimensions
+        cv = acc.computes_cost_volume_mc_cnn_accurate(ref_feature, sec_features, -4, -1, self.sad_cost)
+
+        # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
+        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+
+    def test_computes_cost_volume_mc_cnn_accurate_positive_disp(self):
+        """"
+        Test the computes_cost_volume_mc_cnn_accurate function with positive disparities
+
+        """
+        # create reference and secondary features
+        ref_feature = torch.randn((1, 112, 4, 4), dtype=torch.float64)
+        sec_features = torch.randn((1, 112, 4, 4), dtype=torch.float64)
+
+        # Create the ground truth cost volume (row, col, disp)
+        cv_GT = np.full((4, 4, 4), np.nan)
+
+        # disparity 1
+        cv_GT[:, :3, 0] = self.sad_cost(ref_feature[:, :, :, :3], sec_features[:, :, :, 1:4]).cpu().detach().numpy()
+        # disparity 2
+        cv_GT[:, :2, 1] = self.sad_cost(ref_feature[:, :, :, :2], sec_features[:, :, :, 2:4]).cpu().detach().numpy()
+        # disparity 3
+        cv_GT[:, :1, 2] = self.sad_cost(ref_feature[:, :, :, :1], sec_features[:, :, :, 3:]).cpu().detach().numpy()
+        # disparity 4
+        # all nan
+
+        # The minus sign converts the similarity score to a matching cost
+        cv_GT *= -1
+
+        acc = AccMcCnnTesting()
+        # Because input shape of nn.Conv2d is (Batch_size, Channel, H, W), we add 1 dimensions
+        cv = acc.computes_cost_volume_mc_cnn_accurate(ref_feature, sec_features, 1, 4, self.sad_cost)
 
         # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
         np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
