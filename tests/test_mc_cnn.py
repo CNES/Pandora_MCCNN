cee2633a720d0d7dabcc5eed4e40e8a23a2a33ac
Veronique Defonte <veronique.defonte@c-s.fr>
2021-03-03 09:26:34 +0100

    refactor: pep8 test_mc_cnn snake case naming convention

--- tests/test_mc_cnn.py
+++ tests/test_mc_cnn.py
@@ -45,26 +45,26 @@ class TestMCCNN(unittest.TestCase):
         cos = nn.CosineSimilarity(dim=0, eps=1e-6)
 
         # Create the ground truth cost volume (row, col, disp)
-        cv_GT = np.full((4, 4, 5), np.nan)
+        cv_gt = np.full((4, 4, 5), np.nan)
 
         # disparity -2
-        cv_GT[:, 2:, 0] = cos(ref_feature[:, :, 2:], sec_features[:, :, 0:2]).cpu().detach().numpy()
+        cv_gt[:, 2:, 0] = cos(ref_feature[:, :, 2:], sec_features[:, :, 0:2]).cpu().detach().numpy()
         # disparity -1
-        cv_GT[:, 1:, 1] = cos(ref_feature[:, :, 1:], sec_features[:, :, 0:3]).cpu().detach().numpy()
+        cv_gt[:, 1:, 1] = cos(ref_feature[:, :, 1:], sec_features[:, :, 0:3]).cpu().detach().numpy()
         # disparity 0
-        cv_GT[:, :, 2] = cos(ref_feature[:, :, :], sec_features[:, :, :]).cpu().detach().numpy()
+        cv_gt[:, :, 2] = cos(ref_feature[:, :, :], sec_features[:, :, :]).cpu().detach().numpy()
         # disparity 1
-        cv_GT[:, :3, 3] = cos(ref_feature[:, :, :3], sec_features[:, :, 1:4]).cpu().detach().numpy()
+        cv_gt[:, :3, 3] = cos(ref_feature[:, :, :3], sec_features[:, :, 1:4]).cpu().detach().numpy()
         # disparity 2
-        cv_GT[:, :2, 4] = cos(ref_feature[:, :, :2], sec_features[:, :, 2:4]).cpu().detach().numpy()
+        cv_gt[:, :2, 4] = cos(ref_feature[:, :, :2], sec_features[:, :, 2:4]).cpu().detach().numpy()
 
         # The minus sign converts the similarity score to a matching cost
-        cv_GT *= -1
+        cv_gt *= -1
 
         cv = computes_cost_volume_mc_cnn_fast(ref_feature, sec_features, -2, 2)
 
         # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
-        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+        np.testing.assert_allclose(cv, cv_gt, rtol=1e-05)
 
     def test_computes_cost_volume_mc_cnn_fast_negative_disp(self):
         """"
@@ -78,24 +78,24 @@ class TestMCCNN(unittest.TestCase):
         cos = nn.CosineSimilarity(dim=0, eps=1e-6)
 
         # Create the ground truth cost volume (row, col, disp)
-        cv_GT = np.full((4, 4, 4), np.nan)
+        cv_gt = np.full((4, 4, 4), np.nan)
 
         # disparity -4
         # all nan
         # disparity -3
-        cv_GT[:, 3:, 1] = cos(ref_feature[:, :, 3:], sec_features[:, :, 0:1]).cpu().detach().numpy()
+        cv_gt[:, 3:, 1] = cos(ref_feature[:, :, 3:], sec_features[:, :, 0:1]).cpu().detach().numpy()
         # disparity -2
-        cv_GT[:, 2:, 2] = cos(ref_feature[:, :, 2:], sec_features[:, :, 0:2]).cpu().detach().numpy()
+        cv_gt[:, 2:, 2] = cos(ref_feature[:, :, 2:], sec_features[:, :, 0:2]).cpu().detach().numpy()
         # disparity -1
-        cv_GT[:, 1:, 3] = cos(ref_feature[:, :, 1:], sec_features[:, :, 0:3]).cpu().detach().numpy()
+        cv_gt[:, 1:, 3] = cos(ref_feature[:, :, 1:], sec_features[:, :, 0:3]).cpu().detach().numpy()
 
         # The minus sign converts the similarity score to a matching cost
-        cv_GT *= -1
+        cv_gt *= -1
 
         cv = computes_cost_volume_mc_cnn_fast(ref_feature, sec_features, -4, -1)
 
         # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
-        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+        np.testing.assert_allclose(cv, cv_gt, rtol=1e-05)
 
     def test_computes_cost_volume_mc_cnn_fast_positive_disp(self):
         """"
@@ -109,24 +109,24 @@ class TestMCCNN(unittest.TestCase):
         cos = nn.CosineSimilarity(dim=0, eps=1e-6)
 
         # Create the ground truth cost volume (row, col, disp)
-        cv_GT = np.full((4, 4, 4), np.nan)
+        cv_gt = np.full((4, 4, 4), np.nan)
 
         # disparity 1
-        cv_GT[:, :3, 0] = cos(ref_feature[:, :, :3], sec_features[:, :, 1:4]).cpu().detach().numpy()
+        cv_gt[:, :3, 0] = cos(ref_feature[:, :, :3], sec_features[:, :, 1:4]).cpu().detach().numpy()
         # disparity 2
-        cv_GT[:, :2, 1] = cos(ref_feature[:, :, :2], sec_features[:, :, 2:4]).cpu().detach().numpy()
+        cv_gt[:, :2, 1] = cos(ref_feature[:, :, :2], sec_features[:, :, 2:4]).cpu().detach().numpy()
         # disparity 3
-        cv_GT[:, :1, 2] = cos(ref_feature[:, :, :1], sec_features[:, :, 3:]).cpu().detach().numpy()
+        cv_gt[:, :1, 2] = cos(ref_feature[:, :, :1], sec_features[:, :, 3:]).cpu().detach().numpy()
         # disparity 4
         # all nan
 
         # The minus sign converts the similarity score to a matching cost
-        cv_GT *= -1
+        cv_gt *= -1
 
         cv = computes_cost_volume_mc_cnn_fast(ref_feature, sec_features, 1, 4)
 
         # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
-        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+        np.testing.assert_allclose(cv, cv_gt, rtol=1e-05)
 
     def sad_cost(self, ref_features, sec_features):
         """
@@ -144,28 +144,28 @@ class TestMCCNN(unittest.TestCase):
         sec_features = torch.randn((1, 112, 4, 4), dtype=torch.float64)
 
         # Create the ground truth cost volume (row, col, disp)
-        cv_GT = np.full((4, 4, 5), np.nan)
+        cv_gt = np.full((4, 4, 5), np.nan)
 
         # disparity -2
-        cv_GT[:, 2:, 0] = self.sad_cost(ref_feature[:, :, :, 2:], sec_features[:, :, :, 0:2]).cpu().detach().numpy()
+        cv_gt[:, 2:, 0] = self.sad_cost(ref_feature[:, :, :, 2:], sec_features[:, :, :, 0:2]).cpu().detach().numpy()
         # disparity -1
-        cv_GT[:, 1:, 1] = self.sad_cost(ref_feature[:, :, :, 1:], sec_features[:, :, :, 0:3]).cpu().detach().numpy()
+        cv_gt[:, 1:, 1] = self.sad_cost(ref_feature[:, :, :, 1:], sec_features[:, :, :, 0:3]).cpu().detach().numpy()
         # disparity 0
-        cv_GT[:, :, 2] = self.sad_cost(ref_feature[:, :, :, :], sec_features[:, :, :, :]).cpu().detach().numpy()
+        cv_gt[:, :, 2] = self.sad_cost(ref_feature[:, :, :, :], sec_features[:, :, :, :]).cpu().detach().numpy()
         # disparity 1
-        cv_GT[:, :3, 3] = self.sad_cost(ref_feature[:, :, :, :3], sec_features[:, :, :, 1:4]).cpu().detach().numpy()
+        cv_gt[:, :3, 3] = self.sad_cost(ref_feature[:, :, :, :3], sec_features[:, :, :, 1:4]).cpu().detach().numpy()
         # disparity 2
-        cv_GT[:, :2, 4] = self.sad_cost(ref_feature[:, :, :, :2], sec_features[:, :, :, 2:4]).cpu().detach().numpy()
+        cv_gt[:, :2, 4] = self.sad_cost(ref_feature[:, :, :, :2], sec_features[:, :, :, 2:4]).cpu().detach().numpy()
 
         # The minus sign converts the similarity score to a matching cost
-        cv_GT *= -1
+        cv_gt *= -1
 
         acc = AccMcCnnInfer()
         # Because input shape of nn.Conv2d is (Batch_size, Channel, H, W), we add 1 dimensions
         cv = acc.computes_cost_volume_mc_cnn_accurate(ref_feature, sec_features, -2, 2, self.sad_cost)
 
         # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
-        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+        np.testing.assert_allclose(cv, cv_gt, rtol=1e-05)
 
     def test_computes_cost_volume_mc_cnn_accuratenegative_disp(self):
         """"
@@ -177,26 +177,26 @@ class TestMCCNN(unittest.TestCase):
         sec_features = torch.randn((1, 112, 4, 4), dtype=torch.float64)
 
         # Create the ground truth cost volume (row, col, disp)
-        cv_GT = np.full((4, 4, 4), np.nan)
+        cv_gt = np.full((4, 4, 4), np.nan)
 
         # disparity -4
         # all nan
         # disparity -3
-        cv_GT[:, 3:, 1] = self.sad_cost(ref_feature[:, :, :, 3:], sec_features[:, :, :, 0:1]).cpu().detach().numpy()
+        cv_gt[:, 3:, 1] = self.sad_cost(ref_feature[:, :, :, 3:], sec_features[:, :, :, 0:1]).cpu().detach().numpy()
         # disparity -2
-        cv_GT[:, 2:, 2] = self.sad_cost(ref_feature[:, :, :, 2:], sec_features[:, :, :, 0:2]).cpu().detach().numpy()
+        cv_gt[:, 2:, 2] = self.sad_cost(ref_feature[:, :, :, 2:], sec_features[:, :, :, 0:2]).cpu().detach().numpy()
         # disparity -1
-        cv_GT[:, 1:, 3] = self.sad_cost(ref_feature[:, :, :, 1:], sec_features[:, :, :, 0:3]).cpu().detach().numpy()
+        cv_gt[:, 1:, 3] = self.sad_cost(ref_feature[:, :, :, 1:], sec_features[:, :, :, 0:3]).cpu().detach().numpy()
 
         # The minus sign converts the similarity score to a matching cost
-        cv_GT *= -1
+        cv_gt *= -1
 
         acc = AccMcCnnInfer()
         # Because input shape of nn.Conv2d is (Batch_size, Channel, H, W), we add 1 dimensions
         cv = acc.computes_cost_volume_mc_cnn_accurate(ref_feature, sec_features, -4, -1, self.sad_cost)
 
         # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
-        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+        np.testing.assert_allclose(cv, cv_gt, rtol=1e-05)
 
     def test_computes_cost_volume_mc_cnn_accurate_positive_disp(self):
         """"
@@ -208,26 +208,26 @@ class TestMCCNN(unittest.TestCase):
         sec_features = torch.randn((1, 112, 4, 4), dtype=torch.float64)
 
         # Create the ground truth cost volume (row, col, disp)
-        cv_GT = np.full((4, 4, 4), np.nan)
+        cv_gt = np.full((4, 4, 4), np.nan)
 
         # disparity 1
-        cv_GT[:, :3, 0] = self.sad_cost(ref_feature[:, :, :, :3], sec_features[:, :, :, 1:4]).cpu().detach().numpy()
+        cv_gt[:, :3, 0] = self.sad_cost(ref_feature[:, :, :, :3], sec_features[:, :, :, 1:4]).cpu().detach().numpy()
         # disparity 2
-        cv_GT[:, :2, 1] = self.sad_cost(ref_feature[:, :, :, :2], sec_features[:, :, :, 2:4]).cpu().detach().numpy()
+        cv_gt[:, :2, 1] = self.sad_cost(ref_feature[:, :, :, :2], sec_features[:, :, :, 2:4]).cpu().detach().numpy()
         # disparity 3
-        cv_GT[:, :1, 2] = self.sad_cost(ref_feature[:, :, :, :1], sec_features[:, :, :, 3:]).cpu().detach().numpy()
+        cv_gt[:, :1, 2] = self.sad_cost(ref_feature[:, :, :, :1], sec_features[:, :, :, 3:]).cpu().detach().numpy()
         # disparity 4
         # all nan
 
         # The minus sign converts the similarity score to a matching cost
-        cv_GT *= -1
+        cv_gt *= -1
 
         acc = AccMcCnnInfer()
         # Because input shape of nn.Conv2d is (Batch_size, Channel, H, W), we add 1 dimensions
         cv = acc.computes_cost_volume_mc_cnn_accurate(ref_feature, sec_features, 1, 4, self.sad_cost)
 
         # Check if the calculated cost volume is equal to the ground truth (same shape and all elements equals)
-        np.testing.assert_allclose(cv, cv_GT, rtol=1e-05)
+        np.testing.assert_allclose(cv, cv_gt, rtol=1e-05)
 
     # pylint: disable=invalid-name 
     # -> because changing the name here loses the reference to the actual name of the checked function
@@ -287,22 +287,22 @@ class TestMCCNN(unittest.TestCase):
         x_ref_patch = 6
         y_ref_patch = 5
         patch_size = 5
-        gt_ref_patch = self.ref_img_0[y_ref_patch - patch_size : y_ref_patch + patch_size + 1,
-                                      x_ref_patch - patch_size : x_ref_patch + patch_size + 1]
+        gt_ref_patch = self.ref_img_0[y_ref_patch - patch_size: y_ref_patch + patch_size + 1,
+                                      x_ref_patch - patch_size: x_ref_patch + patch_size + 1]
 
         # disp = 1, with middlebury image convention img_ref(x,y) = img_sec(x-d,y)
         disp = 1
         x_sec_pos_patch = x_ref_patch - disp
         y_sec_pos_patch = 5
-        gt_sec_pos_patch = self.sec_img_0[y_sec_pos_patch - patch_size : y_sec_pos_patch + patch_size + 1,
-                                          x_sec_pos_patch - patch_size : x_sec_pos_patch + patch_size + 1]
+        gt_sec_pos_patch = self.sec_img_0[y_sec_pos_patch - patch_size: y_sec_pos_patch + patch_size + 1,
+                                          x_sec_pos_patch - patch_size: x_sec_pos_patch + patch_size + 1]
 
         # dataset_neg_low & dataset_neg_high = 1, with middlebury image convention img_ref(x,y) = img_sec(x-d,y)
         dataset_neg = 1
         x_sec_neg_patch = x_ref_patch - disp + dataset_neg
         y_sec_neg_patch = 5
-        gt_sec_neg_patch = self.sec_img_0[y_sec_neg_patch - patch_size : y_sec_neg_patch + patch_size + 1,
-                                          x_sec_neg_patch - patch_size : x_sec_neg_patch + patch_size + 1]
+        gt_sec_neg_patch = self.sec_img_0[y_sec_neg_patch - patch_size: y_sec_neg_patch + patch_size + 1,
+                                          x_sec_neg_patch - patch_size: x_sec_neg_patch + patch_size + 1]
 
         gt_path = np.stack((gt_ref_patch, gt_sec_pos_patch, gt_sec_neg_patch), axis=0)
 
@@ -315,22 +315,22 @@ class TestMCCNN(unittest.TestCase):
         x_ref_patch = 5
         y_ref_patch = 7
         patch_size = 5
-        gt_ref_patch = self.ref_img_0[y_ref_patch - patch_size : y_ref_patch + patch_size + 1,
-                                      x_ref_patch - patch_size : x_ref_patch + patch_size + 1]
+        gt_ref_patch = self.ref_img_0[y_ref_patch - patch_size: y_ref_patch + patch_size + 1,
+                                      x_ref_patch - patch_size: x_ref_patch + patch_size + 1]
 
         # disp = -1, with middlebury image convention img_ref(x,y) = img_sec(x-d,y)
         disp = -1
         x_sec_pos_patch = x_ref_patch - disp
         y_sec_pos_patch = 5
-        gt_sec_pos_patch = self.sec_img_0[y_sec_pos_patch - patch_size : y_sec_pos_patch + patch_size + 1,
-                                          x_sec_pos_patch - patch_size : x_sec_pos_patch + patch_size + 1]
+        gt_sec_pos_patch = self.sec_img_0[y_sec_pos_patch - patch_size: y_sec_pos_patch + patch_size + 1,
+                                          x_sec_pos_patch - patch_size: x_sec_pos_patch + patch_size + 1]
 
         # dataset_neg_low & dataset_neg_high = 1, with middlebury image convention img_ref(x,y) = img_sec(x-d,y)
         dataset_neg = 1
         x_sec_neg_patch = x_ref_patch - disp + dataset_neg
         y_sec_neg_patch = 5
-        gt_sec_neg_patch = self.sec_img_0[y_sec_neg_patch - patch_size : y_sec_neg_patch + patch_size + 1,
-                                          x_sec_neg_patch - patch_size : x_sec_neg_patch + patch_size + 1]
+        gt_sec_neg_patch = self.sec_img_0[y_sec_neg_patch - patch_size: y_sec_neg_patch + patch_size + 1,
+                                          x_sec_neg_patch - patch_size: x_sec_neg_patch + patch_size + 1]
 
         gt_path = np.stack((gt_ref_patch, gt_sec_pos_patch, gt_sec_neg_patch), axis=0)
 
@@ -394,8 +394,8 @@ class TestMCCNN(unittest.TestCase):
         disp = 1
         x_sec_pos_patch = x_ref_patch - disp
         y_sec_pos_patch = 5
-        gt_sec_pos_patch = self.sec_img_0[y_sec_pos_patch - patch_size : y_sec_pos_patch + patch_size + 1,
-                                          x_sec_pos_patch - patch_size : x_sec_pos_patch + patch_size + 1]
+        gt_sec_pos_patch = self.sec_img_0[y_sec_pos_patch - patch_size: y_sec_pos_patch + patch_size + 1,
+                                          x_sec_pos_patch - patch_size: x_sec_pos_patch + patch_size + 1]
 
         # dataset_neg_low & dataset_neg_high = 1, with middlebury image convention img_ref(x,y) = img_sec(x-d,y)
         dataset_neg = 1
@@ -414,22 +414,22 @@ class TestMCCNN(unittest.TestCase):
         x_ref_patch = 5
         y_ref_patch = 7
         patch_size = 5
-        gt_ref_patch = self.ref_img_1[y_ref_patch - patch_size : y_ref_patch + patch_size + 1,
-                                      x_ref_patch - patch_size : x_ref_patch + patch_size + 1]
+        gt_ref_patch = self.ref_img_1[y_ref_patch - patch_size: y_ref_patch + patch_size + 1,
+                                      x_ref_patch - patch_size: x_ref_patch + patch_size + 1]
 
         # disp = -1, with middlebury image convention img_ref(x,y) = img_sec(x-d,y)
         disp = -1
         x_sec_pos_patch = x_ref_patch - disp
         y_sec_pos_patch = 7
-        gt_sec_pos_patch = self.sec_img_2[y_sec_pos_patch - patch_size : y_sec_pos_patch + patch_size + 1,
-                                          x_sec_pos_patch - patch_size : x_sec_pos_patch + patch_size + 1]
+        gt_sec_pos_patch = self.sec_img_2[y_sec_pos_patch - patch_size: y_sec_pos_patch + patch_size + 1,
+                                          x_sec_pos_patch - patch_size: x_sec_pos_patch + patch_size + 1]
 
         # dataset_neg_low & dataset_neg_high = 1, with middlebury image convention img_ref(x,y) = img_sec(x-d,y)
         dataset_neg = 1
         x_sec_neg_patch = x_ref_patch - disp + dataset_neg
         y_sec_neg_patch = 7
-        gt_sec_neg_patch = self.sec_img_2[y_sec_neg_patch - patch_size : y_sec_neg_patch + patch_size + 1,
-                                          x_sec_neg_patch - patch_size : x_sec_neg_patch + patch_size + 1]
+        gt_sec_neg_patch = self.sec_img_2[y_sec_neg_patch - patch_size: y_sec_neg_patch + patch_size + 1,
+                                          x_sec_neg_patch - patch_size: x_sec_neg_patch + patch_size + 1]
 
         gt_path = np.stack((gt_ref_patch, gt_sec_pos_patch, gt_sec_neg_patch), axis=0)
 
