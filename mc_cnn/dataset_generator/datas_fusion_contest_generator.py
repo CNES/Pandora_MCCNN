8d5411d58affbc485d3d629ab0f5c8aa6625617d
Veronique Defonte <veronique.defonte@c-s.fr>
2021-03-04 15:55:46 +0100

    refactor: pep8 dataset_generator/datas_fusion_contest_generator.py

--- mc_cnn/dataset_generator/datas_fusion_contest_generator.py
+++ mc_cnn/dataset_generator/datas_fusion_contest_generator.py
@@ -1,18 +1,20 @@
-# coding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019 Centre National d'Etudes Spatiales
+
 """
-:author: Véronique Defonte
-:organization: CS SI
-:copyright: 2020 CNES. All rights reserved.
-:created: Jan. 2020
+This module contains all functions to load and preprocess dataset
 """
 
+import math
 from torch.utils import data
 import numpy as np
 import h5py
 import cv2
-import math
 
 
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-arguments
 class DataFusionContestGenerator(data.Dataset):
     """
     Generate data fusion dataset for training
@@ -65,42 +67,43 @@ class DataFusionContestGenerator(data.Dataset):
         :rtype: np.array(3, patch_size, patch_size)
         """
         # Make patch
-        id = int(self.data[index, 0])
-        y = int(self.data[index, 1])
-        x = int(self.data[index, 2])
+        row = int(self.data[index, 1])
+        col = int(self.data[index, 2])
         disp = int(self.data[index, 3])
-        id_data = self.id_image.index(id)
+        id_data = self.id_image.index(int(self.data[index, 0]))
 
-        w = int(self.patch_size / 2)
+        # Patch radius
+        radius = int(self.patch_size / 2)
+        width = self.image[id_data].shape[2] - radius
 
+        # Column of the positive example
         x_pos = -1
-        width = self.image[id_data].shape[2] - w
-
         while x_pos < 0 or x_pos >= width:
-            x_pos = int((x - disp) + np.random.uniform(-self.pos, self.pos))
+            x_pos = int((col - disp) + np.random.uniform(-self.pos, self.pos))
 
+        # Column of the negative example
         x_neg = -1
         while x_neg < 0 or x_neg >= width:
-            x_neg = int((x - disp) + np.random.uniform(self.neg_low, self.neg_high))
+            x_neg = int((col - disp) + np.random.uniform(self.neg_low, self.neg_high))
 
-        height = self.image[id_data].shape[1] - w
+        height = self.image[id_data].shape[1] - radius
 
         y_disp = -1
         while y_disp < 0 or y_disp >= height:
-            y_disp = int(y + np.random.uniform(-self.disp_vert, self.disp_vert))
+            y_disp = int(row + np.random.uniform(-self.disp_vert, self.disp_vert))
 
         if self.transformation:
             # Calculates random data augmentation
-            s = np.random.uniform(self.scale, 1)
-            scale = [s * np.random.uniform(self.hscale, 1), s]
+            rand_scale = np.random.uniform(self.scale, 1)
+            scale = [rand_scale * np.random.uniform(self.hscale, 1), rand_scale]
             hshear = np.random.uniform(-self.hshear, self.hshear)
             trans = [np.random.uniform(-self.trans, self.trans), np.random.uniform(-self.trans, self.trans)]
             phi = np.random.uniform(-self.rotate * math.pi / 180., self.rotate * math.pi / 180.)
             brightness = np.random.uniform(-self.brightness, self.brightness)
             contrast = np.random.uniform(1. / self.contrast, self.contrast)
 
-            left = self.data_augmentation(self.image[id_data][0, :, :], y, x, scale, phi,
-                                               trans, hshear, brightness, contrast)
+            left = self.data_augmentation(self.image[id_data][0, :, :], row, col, scale, phi,
+                                          trans, hshear, brightness, contrast)
 
             scale__ = [scale[0] * np.random.uniform(self.d_hscale, 1), scale[1]]
             hshear_ = hshear + np.random.uniform(-self.d_hshear, self.d_hshear)
@@ -117,28 +120,27 @@ class DataFusionContestGenerator(data.Dataset):
 
         else:
             # Make the left patch
-            left = self.image[id_data][0, y - w: y + w + 1, x - w: x + w + 1]
+            left = self.image[id_data][0, row - radius: row + radius + 1, col - radius: col + radius + 1]
             # Make the right positive patch
-            right_pos = self.image[id_data][1, y_disp - w: y_disp + w + 1, x_pos - w: w + x_pos + 1]
+            right_pos = self.image[id_data][1, y_disp - radius: y_disp + radius + 1, x_pos - radius: radius + x_pos + 1]
             # Make the right negative patch
-            right_neg = self.image[id_data][1, y_disp - w: y_disp + w + 1, x_neg - w: w + x_neg + 1]
+            right_neg = self.image[id_data][1, y_disp - radius: y_disp + radius + 1, x_neg - radius: radius + x_neg + 1]
 
         return np.stack((left, right_pos, right_neg), axis=0)
 
     def __len__(self):
         """
         Return the total number of samples
-
         """
         return self.data.shape[0]
 
-    def data_augmentation(self, src, y, x, scale, phi, trans, hshear, brightness, contrast):
+    def data_augmentation(self, src, row, col, scale, phi, trans, hshear, brightness, contrast):
         """
-        Return augmented patch
+        Return augmented patch : apply affine transformations
 
         :param src: source image
-        :param y: center of the patch
-        :param x: center of the patch
+        :param row: row center of the patch
+        :param col: col center of the patch
         :param scale: scale factor
         :param phi: rotation factor
         :param trans:translation factor
@@ -146,25 +148,27 @@ class DataFusionContestGenerator(data.Dataset):
         :param brightness: brightness
         :param contrast: contrast
         :return: the augmented patch
-        :rtype: np.array(11, 11)
+        :rtype: np.array(self.patch_size, self.patch_size)
         """
-        m = [1, 0, -x, 0, 1, -y]
-        m = self.mul32([1, 0, trans[0], 0, 1, trans[1]], m)
-        m = self.mul32([scale[0], 0, 0, 0, scale[1], 0], m)
-        c = math.cos(phi)
-        s = math.sin(phi)
-        m = self.mul32([c, s, 0, -s, c, 0], m)
-        m = self.mul32([1, hshear, 0, 0, 1, 0], m)
-        m = self.mul32([1, 0, (self.patch_size - 1) / 2, 0, 1, (self.patch_size - 1) / 2], m)
+        homo_matrix = np.array([[1, 0, -col], [0, 1, -row], [0, 0, 1]])
+        translation_matrix = np.array([[1, 0, trans[0]], [0, 1, trans[1]], [0, 0, 1]])
+        homo_matrix = np.matmul(translation_matrix, homo_matrix)
 
-        m = np.reshape(m, (2,3))
+        scale_matrix = np.array([[scale[0], 0, 0], [0, scale[1], 0], [0, 0, 1]])
+        homo_matrix = np.matmul(scale_matrix, homo_matrix)
 
-        dst = cv2.warpAffine(src, m, (self.patch_size, self.patch_size))
+        cos_phi = math.cos(phi)
+        sin_phi = math.sin(phi)
+        rotate_matrix = np.array([[cos_phi, sin_phi, 0], [-sin_phi, cos_phi, 0], [0, 0, 1]])
+        homo_matrix = np.matmul(rotate_matrix, homo_matrix)
+
+        shear_matrix = np.array([[1, hshear, 0], [0, 1, 0], [0, 0, 1]])
+        homo_matrix = np.matmul(shear_matrix, homo_matrix)
+
+        translation_matrix = np.array([[1, 0, (self.patch_size - 1) / 2], [0, 1, (self.patch_size - 1) / 2]])
+        homo_matrix = np.matmul(translation_matrix, homo_matrix)
+
+        dst = cv2.warpAffine(src, homo_matrix, (self.patch_size, self.patch_size))
         dst *= contrast
         dst += brightness
         return dst
-
-    def mul32(self, a, b):
-        return a[0]*b[0]+a[1]*b[3], a[0]*b[1]+a[1]*b[4], a[0]*b[2]+a[1]*b[5]+a[2], a[3]*b[0]+a[4]*b[3], \
-               a[3]*b[1]+a[4]*b[4], a[3]*b[2]+a[4]*b[5]+a[5]
-
