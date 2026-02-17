ffa6180e3343f2c12a9f90ccc5c5ef999e44dd9f
Pauline Delporte <pauline.delporte@cs-soprasteria.com>
2026-02-17 16:51:56 +0000

    feat: add onnx files

--- mc_cnn/weights/weights.py
+++ mc_cnn/weights/weights.py
@@ -35,7 +35,7 @@ AVAILABLE_WEIGHTS = {
     "fast": {"middlebury": "mc_cnn_fast_mb_weights.pt", "dfc": "mc_cnn_fast_data_fusion_contest.pt"},
     "accurate": {"middlebury": "mc_cnn_accurate_mb_weights.pt", "dfc": "mc_cnn_accurate_data_fusion_contest.pt"},
     "onnx_int8": {"middlebury": "mc_cnn_fast_mb_weights_dynamo_int8_excl_01.onnx"},
-    "onnx_dw": {"middlebury": "mc_cnn_fast_epoch13.onnx"},
+    "onnx_dw": {"middlebury": "mc_cnn_fast_dw.onnx"},
 }
 
 
